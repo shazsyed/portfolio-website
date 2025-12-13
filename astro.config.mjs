2bb9d94287e20df05523c0e9f6cf963f4dad4459
Shaz Syed Ali <shazsyed1@hotmail.com>
2025-12-13 16:48:39 +0500

    feat: Add resume download link and update footer copyright information

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  redirects: {
+    '/resume': 'https://drive.google.com/file/d/1xQt3q4lHz3YwcZadBPUryet8kKj4eTmA/view'
+  },
   build: {
     format: 'file',
     assets: '_astro'
